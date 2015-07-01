79ed388c72efdb4416e448c206448d7ec09a14b1
deidigitalis <deidigitalis@gmail.com>
2015-07-01 11:32:31 +0200

    Assembly settings
    Fized version to 0.1
    Fixed assembly names,

--- TntBot/Properties/AssemblyInfo.cs
+++ TntBot/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TntBot")]
+[assembly: AssemblyTitle("Tnt Bot")]
 [assembly: AssemblyDescription("TNT Bot - Photo Web Crawler")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("deidigitalis")]
-[assembly: AssemblyProduct("TntBot")]
+[assembly: AssemblyProduct("TNT Bot")]
 [assembly: AssemblyCopyright("GPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -46,5 +46,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
